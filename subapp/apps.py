3d4998d7a8f608f8c5dad4db4ac8bfc23c6cb999
roychen7 <roychen0724@hotmail.com>
2021-06-05 21:50:54 -0700

    changes

--- subapp/apps.py
+++ subapp/apps.py
@@ -2,7 +2,3 @@ from django.apps import AppConfig
 
 class SubappConfig(AppConfig):
     name = 'subapp'
-
-    def ready(self):
-        from django_client_framework.permissions import reset_permissions
-        reset_permissions()
--- subapp/views.py
+++ subapp/views.py
@@ -3,16 +3,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from subapp.models import Product, Brand
 from subprocess import Popen, run, SubprocessError, PIPE
-from django_client_framework.permissions import default_groups, add_perms_shortcut
+from django_client_framework.permissions import default_groups, add_perms_shortcut, reset_permissions
 from django.core.cache import cache
 
 # Create your views here.
 def clear(request):
     for cmd in [
         'python3 manage.py flush --no-input',
+#         """
+# from django_client_framework.permissions import reset_permissions
+# reset_permissions()
+# """
     ]: shell(cmd)
     # shell('python3 manage.py flush --no-input')
     cache.clear()
+    reset_permissions()
     add_perms_shortcut(default_groups.anyone, Product, "rwcd")
     add_perms_shortcut(default_groups.anyone, Brand, "rwcd")
     return HttpResponse('Successfully deleted all')
