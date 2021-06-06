debfd817289ecb6b041bfb68c3c100dcbcd6ddb9
roychen7 <roychen0724@hotmail.com>
2021-06-05 21:34:11 -0700

    update

--- subapp/apps.py
+++ subapp/apps.py
@@ -1,12 +1,11 @@
 from django.apps import AppConfig
-from django_client_framework.permissions import default_groups, add_perms_shortcut
 
 class SubappConfig(AppConfig):
     name = 'subapp'
 
     def ready(self):
-        pass
-        # from .models import Product, Brand
+        from django_client_framework.permissions import default_groups, add_perms_shortcut
+        from .models import Product, Brand
         
-        # add_perms_shortcut(default_groups.anyone, Product, "rwcd")
-        # add_perms_shortcut(default_groups.anyone, Brand, "rwcd")
+        add_perms_shortcut(default_groups.anyone, Product, "rwcd")
+        add_perms_shortcut(default_groups.anyone, Brand, "rwcd")
