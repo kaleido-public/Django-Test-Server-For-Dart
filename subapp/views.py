5f2b267fbf7be16f0c3271f80704befbdbf9bef1
Yu Li <ylilarry@gmail.com>
2021-08-03 22:38:29 -0400

    clear up

--- subapp/views.py
+++ subapp/views.py
@@ -6,21 +6,14 @@ from django_client_framework.permissions import (
     add_perms_shortcut,
     reset_permissions,
 )
-from django.core.cache import cache
-from django.db import transaction
 
 
-# Create your views here.
-# @transaction.atomic
 def clear(request):
-    # Product.objects.select_for_update().delete()
-    # Brand.objects.select_for_update().delete()
     shell("python3 manage.py flush --no-input")
-    cache.clear()
     reset_permissions()
     add_perms_shortcut(default_groups.anyone, Product, "rwcd")
     add_perms_shortcut(default_groups.anyone, Brand, "rwcd")
-    return HttpResponse("Successfully deleted all")
+    return HttpResponse("Successfully deleted all.")
 
 
 def shell(cmd, **kwargs):
